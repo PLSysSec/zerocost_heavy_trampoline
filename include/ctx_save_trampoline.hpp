c35d0af03aa2fbb2919ae927e00f1425e0e07a4c
shravanrn@gmail.com <shravanrn@gmail.com>
2021-04-05 17:13:42 -0700

    Correctly handle class params and class returns

--- include/ctx_save_trampoline.hpp
+++ include/ctx_save_trampoline.hpp
@@ -15,6 +15,60 @@ namespace switch_execution_detail {
 template<typename T>
 constexpr bool false_v = false;
 
+  ///////////////////////////////////////////////////////////////
+
+  // https://stackoverflow.com/questions/6512019/can-we-get-the-type-of-a-lambda-argument
+  namespace return_argument_detail {
+    template<typename Ret, typename... Rest>
+    Ret helper(Ret (*)(Rest...));
+
+    template<typename Ret, typename F, typename... Rest>
+    Ret helper(Ret (F::*)(Rest...));
+
+    template<typename Ret, typename F, typename... Rest>
+    Ret helper(Ret (F::*)(Rest...) const);
+
+    template<typename F>
+    decltype(helper(&F::operator())) helper(F);
+  } // namespace return_argument_detail
+
+  template<typename T>
+  using return_argument =
+    decltype(return_argument_detail::helper(std::declval<T>()));
+
+///////////////////////////////////////////////////////////////
+
+  namespace prepend_arg_type_detail {
+    template<typename T, typename T_ArgNew>
+    struct helper;
+
+    template<typename T_ArgNew, typename T_Ret, typename... T_Args>
+    struct helper<T_Ret(T_Args...), T_ArgNew>
+    {
+      using type = T_Ret(T_ArgNew, T_Args...);
+    };
+  }
+
+  template<typename T_Func, typename T_ArgNew>
+  using prepend_arg_type =
+    typename prepend_arg_type_detail::helper<T_Func, T_ArgNew>::type;
+
+  ///////////////////////////////////////////////////////////////
+
+  namespace change_return_type_detail {
+    template<typename T, typename T_RetNew>
+    struct helper;
+
+    template<typename T_RetNew, typename T_Ret, typename... T_Args>
+    struct helper<T_Ret(T_Args...), T_RetNew>
+    {
+      using type = T_RetNew(T_Args...);
+    };
+  }
+
+  template<typename T_Func, typename T_RetNew>
+  using change_return_type =
+    typename change_return_type_detail::helper<T_Func, T_RetNew>::type;
 }
 
 extern "C"
@@ -65,29 +119,31 @@ private:
                                      T_ActualArgs&&... args)
   {
     T_FormalArg arg_conv = arg;
-    uint64_t val = serialize_to_uint64(arg_conv);
 
     if constexpr (std::is_integral_v<T_FormalArg> ||
                   std::is_pointer_v<T_FormalArg> ||
                   std::is_reference_v<T_FormalArg> ||
                   std::is_enum_v<T_FormalArg>) {
 
-      if constexpr (T_IntegerNum == 0) {
-        ctx->rdi = val;
-      } else if constexpr (T_IntegerNum == 1) {
-        ctx->rsi = val;
-      } else if constexpr (T_IntegerNum == 2) {
-        ctx->rdx = val;
-      } else if constexpr (T_IntegerNum == 3) {
-        ctx->rcx = val;
-      } else if constexpr (T_IntegerNum == 4) {
-        ctx->r8 = val;
-      } else if constexpr (T_IntegerNum == 5) {
-        ctx->r9 = val;
+      if constexpr (T_IntegerNum < 6) {
+        uint64_t val = serialize_to_uint64(arg_conv);
+        if constexpr (T_IntegerNum == 0) {
+          ctx->rdi = val;
+        } else if constexpr (T_IntegerNum == 1) {
+          ctx->rsi = val;
+        } else if constexpr (T_IntegerNum == 2) {
+          ctx->rdx = val;
+        } else if constexpr (T_IntegerNum == 3) {
+          ctx->rcx = val;
+        } else if constexpr (T_IntegerNum == 4) {
+          ctx->r8 = val;
+        } else if constexpr (T_IntegerNum == 5) {
+          ctx->r9 = val;
+        }
       } else {
-        memcpy(target_buffer, &val, sizeof(val));
-        target_buffer += sizeof(val);
-        target_buffer_used_size += sizeof(val);
+        memcpy(target_buffer, &arg_conv, sizeof(arg_conv));
+        target_buffer += sizeof(arg_conv);
+        target_buffer_used_size += sizeof(arg_conv);
       }
 
       return push_parameters<T_IntegerNum + 1, T_FloatNum>(
@@ -100,26 +156,29 @@ private:
     } else if constexpr (std::is_same_v<T_FormalArg, float> ||
                          std::is_same_v<T_FormalArg, double>) {
 
-      if constexpr (T_FloatNum == 0) {
-        ctx->xmm0 = val;
-      } else if constexpr (T_FloatNum == 1) {
-        ctx->xmm1 = val;
-      } else if constexpr (T_FloatNum == 2) {
-        ctx->xmm2 = val;
-      } else if constexpr (T_FloatNum == 3) {
-        ctx->xmm3 = val;
-      } else if constexpr (T_FloatNum == 4) {
-        ctx->xmm4 = val;
-      } else if constexpr (T_FloatNum == 5) {
-        ctx->xmm5 = val;
-      } else if constexpr (T_FloatNum == 6) {
-        ctx->xmm6 = val;
-      } else if constexpr (T_FloatNum == 7) {
-        ctx->xmm7 = val;
+      if constexpr (T_FloatNum < 8) {
+        uint64_t val = serialize_to_uint64(arg_conv);
+        if constexpr (T_FloatNum == 0) {
+          ctx->xmm0 = val;
+        } else if constexpr (T_FloatNum == 1) {
+          ctx->xmm1 = val;
+        } else if constexpr (T_FloatNum == 2) {
+          ctx->xmm2 = val;
+        } else if constexpr (T_FloatNum == 3) {
+          ctx->xmm3 = val;
+        } else if constexpr (T_FloatNum == 4) {
+          ctx->xmm4 = val;
+        } else if constexpr (T_FloatNum == 5) {
+          ctx->xmm5 = val;
+        } else if constexpr (T_FloatNum == 6) {
+          ctx->xmm6 = val;
+        } else if constexpr (T_FloatNum == 7) {
+          ctx->xmm7 = val;
+        }
       } else {
-        memcpy(target_buffer, &val, sizeof(val));
-        target_buffer += sizeof(val);
-        target_buffer_used_size += sizeof(val);
+        memcpy(target_buffer, &arg_conv, sizeof(arg_conv));
+        target_buffer += sizeof(arg_conv);
+        target_buffer_used_size += sizeof(arg_conv);
       }
 
       return push_parameters<T_IntegerNum, T_FloatNum + 1>(
@@ -129,15 +188,25 @@ private:
         reinterpret_cast<T_Ret (*)(T_FormalArgs...)>(0),
         std::forward<T_ActualArgs>(args)...);
     } else {
-      static_assert(switch_execution_detail::false_v<T_Ret>, "Unknown case");
+      memcpy(target_buffer, &arg_conv, sizeof(arg_conv));
+      target_buffer += sizeof(arg_conv);
+      target_buffer_used_size += sizeof(arg_conv);
+
+      return push_parameters<T_IntegerNum, T_FloatNum>(
+        ctx,
+        target_buffer,
+        target_buffer_used_size,
+        reinterpret_cast<T_Ret (*)(T_FormalArgs...)>(0),
+        std::forward<T_ActualArgs>(args)...);
     }
   }
 
   bool m_switch_stacks;
   bool m_windows_mode;
-  char* sandbox_stack_pointer = 0;
-  char* curr_sandbox_stack_pointer = 0;
-  char* parameter_buffer = 0;
+  char* sandbox_stack_pointer = nullptr;
+  char* curr_sandbox_stack_pointer = nullptr;
+  char* parameter_buffer = nullptr;
+  char* return_slot = nullptr;
 
 public:
   void init(bool switch_stacks, bool windows_mode)
@@ -158,11 +227,12 @@ public:
       curr_sandbox_stack_pointer -=
         (reinterpret_cast<uintptr_t>(curr_sandbox_stack_pointer) % 16);
     }
-    // picking a parameter buffer of 4k arbitrarily. This should be sufficiently large for most things
-    const uint64_t parameter_buffer_size = 16 * 1024 * 1024;
-    parameter_buffer = new char[parameter_buffer_size];
-    if (parameter_buffer == nullptr) {
-      printf("Could not allocate parameter_buffer\n");
+    // picking a parameter and return buffer of 4k arbitrarily. This should be sufficiently large for most things
+    const uint64_t buffer_size = 16 * 1024 * 1024;
+    parameter_buffer = new char[buffer_size];
+    return_slot = new char[buffer_size];
+    if (parameter_buffer == nullptr || return_slot == nullptr) {
+      printf("Could not allocate parameter or return buffers\n");
       abort();
     }
   }
@@ -172,13 +242,12 @@ public:
     if (sandbox_stack_pointer != nullptr) {
       delete[] sandbox_stack_pointer;
     }
-    if (parameter_buffer != nullptr) {
-      delete[] parameter_buffer;
-    }
+    delete[] parameter_buffer;
+    delete[] return_slot;
   }
 
   template<typename T_Ret, typename... T_FormalArgs, typename... T_ActualArgs>
-  inline T_Ret func_call(T_Ret (*fn_ptr)(T_FormalArgs...), T_ActualArgs&&... args)
+  inline T_Ret func_call_no_class_ret(T_Ret (*fn_ptr)(T_FormalArgs...), T_ActualArgs&&... args)
   {
     char* stack_pointer = nullptr;
     TransitionContext transition_in = {};
@@ -223,8 +292,9 @@ public:
     } else if constexpr (std::is_same_v<T_Ret, void>) {
       return;
     } else if constexpr (sizeof(T_Ret) > 16) {
-      // This doesn't handle struct returns that are not 8 or 16 bytes, but
-      // looks like we don't need that
+      // Should not occur as class returns have already been handled
+      static_assert(switch_execution_detail::false_v<T_Ret>,
+                    "Class return handling error");
       abort();
     } else if constexpr (sizeof(T_Ret) > 8 && sizeof(T_Ret) <= 16) {
       uint64_t result[] = { transition_in.rax, transition_in.rdx };
@@ -236,4 +306,22 @@ public:
       return (T_Ret)transition_in.rax;
     }
   }
+
+  template<typename T_Ret, typename... T_FormalArgs, typename... T_ActualArgs>
+  inline T_Ret func_call(T_Ret (*fn_ptr)(T_FormalArgs...), T_ActualArgs&&... args) {
+    // According to the x86 abi, class returns are processed as an out parameter before the actual function parameters
+    if constexpr (std::is_class_v<T_Ret>) {
+      using T_Conv = void(*)(char*, T_FormalArgs...);
+      auto func_ptr_conv =
+        reinterpret_cast<T_Conv>(reinterpret_cast<uintptr_t>(fn_ptr));
+      func_call_no_class_ret(func_ptr_conv, return_slot, std::forward<T_ActualArgs>(args)...);
+
+      auto ptr = reinterpret_cast<T_Ret*>(return_slot);
+      T_Ret ret = *ptr;
+      return ret;
+    } else {
+      return func_call_no_class_ret(fn_ptr, std::forward<T_ActualArgs>(args)...);
+    }
+  }
+
 };
